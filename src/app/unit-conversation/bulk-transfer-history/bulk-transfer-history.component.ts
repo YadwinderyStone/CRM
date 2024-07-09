3d059369f96830a0b226f2b4350cab7d196605a4
yadwinder <angulardeveloper.india@yellowstonexps.com>
2024-07-09 23:01:19 +0530

    miner fixes

--- src/app/unit-conversation/bulk-transfer-history/bulk-transfer-history.component.ts
+++ src/app/unit-conversation/bulk-transfer-history/bulk-transfer-history.component.ts
@@ -10,7 +10,7 @@ import { ToastrService } from 'ngx-toastr';
 export class BulkTransferHistoryComponent implements OnInit {
   bulkCloserHistoryList: any = [];
   isLoading: boolean = false
-  columnsToDisplay = ['name', 'subStatus', 'fileName', 'createdBy', 'createdDate', 'totalRecords', 'updateRecords', 'comment', 'WithCategoryAndSubCategory', 'withProblemId'];
+  columnsToDisplay = ['name','fileName', 'createdBy', 'createdDate', 'totalRecords', 'updateRecords', 'comment', 'WithCategoryAndSubCategory', 'withProblemId'];
   constructor(
     private interactionCategoryService: InteractionCategoryService,
     private toastrService: ToastrService,
